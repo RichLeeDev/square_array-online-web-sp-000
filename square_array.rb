ca12f8355b705ec3d50650f5e1818b569ceef4a0
Richard Lee <richlee81192@gmail.com>
2019-09-18 00:11:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,10 @@
 def square_array(array)
+<<<<<<< HEAD
   arr = []
   array.each { |i| arr << i ** 2 }
   arr
 end
+=======
+ array.each { |index| index.square}
+end
+>>>>>>> ace52ff95648cde75ec7cb57b62f61aceab546ca
